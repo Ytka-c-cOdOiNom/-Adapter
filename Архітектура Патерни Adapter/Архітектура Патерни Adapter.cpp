801223456e308bfdf74cc1d77c693742693f2f8d
egor <egor@TOZKAS>
2024-11-10 18:12:52 +0200

    Добавьте файлы проекта.

---
+++ Архітектура Патерни Adapter/Архітектура Патерни Adapter.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+// Старий інтерфейс
+class OldDevice {
+public:
+    void connect() const {
+        cout << "Підключення до старого пристрою" << endl;
+    }
+
+    void disconnect() const {
+        cout << "Відключення від старого пристрою" << endl;
+    }
+};
+
+// Новий інтерфейс
+class NewDeviceInterface {
+public:
+    virtual void start() const = 0;
+    virtual void stop() const = 0;
+    virtual ~NewDeviceInterface() = default;
+};
+
+// Адаптер, що робить старий інтерфейс сумісним з новим
+class DeviceAdapter : public NewDeviceInterface {
+private:
+    const OldDevice& oldDevice;
+
+public:
+    DeviceAdapter(const OldDevice& device) : oldDevice(device) {}
+
+    void start() const override {
+        oldDevice.connect();
+    }
+
+    void stop() const override {
+        oldDevice.disconnect();
+    }
+};
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    OldDevice oldDevice;
+    DeviceAdapter adaptedDevice(oldDevice);
+
+    cout << "Використання адаптованого пристрою через новий інтерфейс:" << endl;
+    adaptedDevice.start();
+    adaptedDevice.stop();
+
+}
+
